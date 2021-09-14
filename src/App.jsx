4c779212a72785fc2795e3ec23a0a4c91c30f3b2
johnnysee <js.johnny.see@gmail.com>
2021-09-14 16:10:46 +0200

    Added Class component to App.jsx and .gitkeep to cypress integration folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return <div></div>;
-};
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
