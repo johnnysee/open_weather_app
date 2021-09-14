bc1eae7deeb99410c5e9983608256a27f5ad6ce2
johnnysee <js.johnny.see@gmail.com>
2021-09-14 16:59:45 +0200

    Implementing the axios.get for location and weather response

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import axios from "axios";
 import React, { Component } from "react";
 
 export class App extends Component {
   state = {
-    geolocation: {}
-  }
+    geolocation: {},
+  };
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(position => {
-      this.setState({ geolocation: position.coords})
-    })
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      let { latitude, longitude } = position.coords;
+      let apiKeyOc = "40200b4156ae4d36ba26a96d811d8560";
+      let apiKeyOw = "ad34bf60b6f7739d2a7e1cd9cd4bb854";
+      let locationResponse = await axios.get(
+        `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKeyOc}`
+      );
+      let weatherResponse = await axios.get(
+        `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKeyOw}`
+      );
+      debugger;
+    });
   }
 
   render() {
