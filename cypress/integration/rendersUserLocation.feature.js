d6304c250e36e2fdc5b77f706825d7cd4a44be5a
johnnysee <js.johnny.see@gmail.com>
2021-09-14 16:32:48 +0200

    Added onBeforeLoad, stubs in rendersUserLocation.feature.js

--- cypress/integration/rendersUserLocation.feature.js
+++ cypress/integration/rendersUserLocation.feature.js
@@ -1,6 +1,20 @@
 describe("weather info for user's location", () => {
   it("is expected to be ", () => {
-    cy.visit("/");
+    cy.visit("/", {
+      onBeforeLoad(window) {
+        const stubLocation = {
+          coords: {
+            latitude: 55.7842,
+            longitude: 12.4518,
+          },
+        };
+        cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
+          (callback) => {
+            return callback(stubLocation);
+          }
+        );
+      },
+    });
     cy.get("[data-cy=weather-display]").within(() => {
       cy.get("[data-cy=temp").should("contain", "17°C");
       cy.get("[data-cy=location]").should("contain", "Virum");
