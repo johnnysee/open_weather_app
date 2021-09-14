91bbfab15e9dbcc3ee259f421681ba40df89bcb6
maxarvid <max.arvid.anderson@gmail.com>
2021-09-14 16:41:50 +0200

    adds componentDidMount function to App.jsx Checks for window geolocation coords

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
 export class App extends Component {
+  state = {
+    geolocation: {}
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({ geolocation: position.coords})
+    })
+  }
+
   render() {
     return (
       <div>
